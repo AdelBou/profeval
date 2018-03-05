f136544a650964bd5dea9fdd674923060255d937
bounouamustapha <ed_dahmane@esi.dz>
2018-03-05 20:21:02 +0100

    Initial commit

---
+++ src/main/java/com/jmasters/profeval/ProfevalApplication.java
@@ -0,0 +1,12 @@
+package com.jmasters.profeval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfevalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfevalApplication.class, args);
+	}
+}
