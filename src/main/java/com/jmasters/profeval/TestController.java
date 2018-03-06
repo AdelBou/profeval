1e73860d6b9c0c73840f9f319d4e3f2a92e670c5
bounouamustapha <ed_dahmane@esi.dz>
2018-03-06 14:00:06 +0100

    zineb

--- src/main/java/com/jmasters/profeval/TestController.java
+++ src/main/java/com/jmasters/profeval/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping
     public String home(){
-        return "this is porfval";
+        return "this is zineb";
     }
 }
